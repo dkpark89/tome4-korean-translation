f1ef54dc3b7158f9913c6547080750ee3dbac703
jinhlk <jinhlk@gmail.com>
2012-12-08 00:41:46 +0000

    지식 습득 관련 오류 수정

--- tome-krtr-b43.teaa/overload/mod/dialogs/ShowLore.lua
+++ tome-krtr-b43.teaa/overload/mod/dialogs/ShowLore.lua
@@ -42,8 +42,8 @@ function _M:init(title, actor)
 
 	self.c_list = ListColumns.new{width=math.floor(self.iw / 2 - 10), height=self.ih - 10, scrollbar=true, sortable=true, columns={
 		{name="", width={40,"fixed"}, display_prop="order", sort="order"},
-		{name="지식", width=60, display_prop="kr_name", sort="kr_name"},
-		{name="종류", width=40, display_prop="kr_cat", sort="cat"},
+		{name="지식", width=60, display_prop="name", sort="name"},
+		{name="종류", width=40, display_prop="cat", sort="cat"},
 	}, list=self.list, fct=function(item) end, select=function(item, sel) self:select(item) end}
 
 	self:loadUI{
@@ -69,7 +69,7 @@ function _M:generateList()
 		--@@
 		local ln = l.kr_display_name
 		if ln == nil or type(ln) ~= "string" then ln = l.name end
-		list[#list+1] = { kr_name=ln, name=l.name, desc=util.getval(l.lore), kr_cat=l.categoty:krLoreCategoty(), cat=l.category, order=l.order, image=l.image }
+		list[#list+1] = { name=ln, desc=util.getval(l.lore), cat=l.category:krLoreCategory(), order=l.order, image=l.image }
 		i = i + 1
 	end
 	-- Add known artifacts
@@ -79,7 +79,7 @@ end
 
 function _M:select(item)
 	if item then
-		self.c_desc:switchItem(item, ("#GOLD#종류:#AQUAMARINE# %s\n      (%s)\n#GOLD#지식 이름:#0080FF# %s\n           (%s)\n#GOLD#내용:#ANTIQUE_WHITE# %s"):format(item.kr_cat, item.cat, item.kr_name, item.name, item.desc))
+		self.c_desc:switchItem(item, ("#GOLD#종류:#AQUAMARINE# %s\n#GOLD#지식 이름:#0080FF# %s\n#GOLD#내용:#ANTIQUE_WHITE# %s"):format(item.cat, item.name, item.desc))
 		if item.image then
 			if type(item.image) == "string" then
 				self.image = Image.new{file="lore/"..item.image, auto_width=true, auto_height=true}
