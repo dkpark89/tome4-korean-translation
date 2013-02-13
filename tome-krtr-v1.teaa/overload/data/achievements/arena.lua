14d9bbd1834b762a7c5ce0d6429caa0222b6aeff
ctcritica93 <ctcritica93@gmail.com>
2013-02-13 03:53:21 +0000

    일단 테스트로 아레나와 이벤트 개인기록란을 번역해둠.

--- tome-krtr-v1.teaa/overload/data/achievements/arena.lua
+++ tome-krtr-v1.teaa/overload/data/achievements/arena.lua
@@ -20,47 +20,55 @@
 newAchievement{
 	name = "The Arena",
 	show = "full",
-	desc = [[Unlocked Arena mode.]],
+	kr_display_name = "아레나 (The Arena)",
+	desc = [[아레나 모드를 해금.]],
 }
 
 newAchievement{
 	name = "Arena Battler 20",
 	show = "full",
-	desc = [[Got to wave 20 in the arena.]],
+	kr_display_name = "아레나의 검투사 - 20 (Arena Battler 20)",
+	desc = [[아레나에서 웨이브 20대에 진입.]],
 }
 
 newAchievement{
 	name = "Arena Battler 50",
 	show = "full",
-	desc = [[Got to wave 50 in the arena.]],
+	kr_display_name = "아레나의 검투사 - 50 (Arena Battler 50)",
+	desc = [[아레나에서 웨이브 50대에 진입.]],
 }
 
 newAchievement{
 	name = "Almost Master of Arena",
 	show = "full",
-	desc = [[Became the new master of the arena in 30-wave mode.]],
+	kr_display_name = "예비 아레나의 마스터 (Almost Master of Arena)",
+	desc = [[30-웨이브 모드에서 아레나의 새로운 마스터가 된다.]],
 }
 
 newAchievement{
 	name = "Master of Arena",
 	show = "full",
-	desc = [[Became the new master of the arena in 60-wave mode.]],
+	kr_display_name = "아레나의 마스터 (Master of Arena)",
+	desc = [[60-웨이브 모드에서 아레나의 새로운 마스터가 된다.]],
 }
 
 newAchievement{
 	name = "XXX the Destroyer",
 	show = "full",
-	desc = [[Earned the rank of Destroyer in the arena.]],
+	kr_display_name = "파괴자 XXX (XXX the Destroyer)",
+	desc = [[아레나에서 파괴자의 등급에 등극한다.]],
 }
 
 newAchievement{
 	name = "Grand Master",
 	show = "full",
-	desc = [[Earned the rank of Grand Master in the arena.]],
+	kr_display_name = "그랜드 마스터 (Grand Master)",
+	desc = [[아레나에서 그랜드 마스터의 등급에 등극한다.]],
 }
 
 newAchievement{
 	name = "Ten at one blow",
 	show = "full",
-	desc = [[Killed 10 or more enemies in one single attack in the arena.]],
+	kr_display_name = "일석십조",
+	desc = [[아레나에서 10마리나 그 이상의 적을 한번의 공격으로 처리한다.]],
 }
