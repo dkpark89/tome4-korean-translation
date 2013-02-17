3b3c0a5bc59d5a567f822ec8c4359623621cf54e
ctcritica93@gmail.com <ctcritica93@gmail.com@c92dd2d4-de0a-8354-a0ac-2da9ae4e0dc5>
2013-02-17 12:58:35 +0000

    몇가지 대사집과 기타등등을 일단 간단히 번역함

--- trunk/tome-krtr-v1.teaa/overload/data/chats/jewelry-store.lua
+++ trunk/tome-krtr-v1.teaa/overload/data/chats/jewelry-store.lua
@@ -18,33 +18,33 @@
 -- darkgod@te4.org
 
 local imbue_ring = function(npc, player)
-	player:showInventory("Imbue which ring?", player:getInven("INVEN"), function(o) return o.type == "jewelry" and o.subtype == "ring" and not o.egoed and not o.unique and not o.rare end, function(ring, ring_item)
-		player:showInventory("Use which gem?", player:getInven("INVEN"), function(gem) return gem.type == "gem" and (gem.material_level or 99) <= ring.material_level and gem.imbue_powers end, function(gem, gem_item)
+	player:showInventory("어떤 반지에 보석을 주입시키실 겁니까?", player:getInven("INVEN"), function(o) return o.type == "jewelry" and o.subtype == "ring" and not o.egoed and not o.unique and not o.rare end, function(ring, ring_item)
+		player:showInventory("어떤 보석을 사용하실 겁니까?", player:getInven("INVEN"), function(gem) return gem.type == "gem" and (gem.material_level or 99) <= ring.material_level and gem.imbue_powers end, function(gem, gem_item)
 			local price = 10 + gem.material_level * 5 + ring.material_level * 7
-			if price > player.money then require("engine.ui.Dialog"):simplePopup("Not enough money", "This costs "..price.." gold, you need more gold.") return end
+			if price > player.money then require("engine.ui.Dialog"):simplePopup("돈이 부족하군요", "골드가 "..price.." 정도가 되면 돌아오시지요, 아직 골드가 좀 부족하군요.") return end
 
-			require("engine.ui.Dialog"):yesnoPopup("Imbue cost", "This will cost you "..price.." gold, do you accept?", function(ret) if ret then
+			require("engine.ui.Dialog"):yesnoPopup("보석 주입 가격", "제가 보아하니 이건 "..price.." 골드 정도가 들겠군요, 보석을 주입하실겁니까?", function(ret) if ret then
 				player:incMoney(-price)
 				player:removeObject(player:getInven("INVEN"), gem_item)
 				ring.wielder = ring.wielder or {}
 				table.mergeAdd(ring.wielder, gem.imbue_powers, true)
-				ring.name = gem.name .. " ring"
+				ring.name = gem.name .. " 반지"
 				ring.been_imbued = true
 				ring.egoed = true
-				game.logPlayer(player, "%s creates: %s", npc.name:capitalize(), ring:getName{do_colour=true, no_count=true})
+				game.logPlayer(player, "%s 가 반지에 보석을 주입하여 만듬: %s", npc.name:capitalize(), ring:getName{do_colour=true, no_count=true})
 			end end, "예", "아니오")
 		end)
 	end)
 end
 
 local artifact_imbue_amulet = function(npc, player)
-	player:showInventory("Imbue which amulet?", player:getInven("INVEN"), function(o) return o.type == "jewelry" and o.subtype == "amulet" and not o.egoed and not o.unique and not o.rare end, function(amulet, amulet_item)
-		player:showInventory("Use which first gem?", player:getInven("INVEN"), function(gem1) return gem1.type == "gem" and (gem1.material_level or 99) <= amulet.material_level and gem1.imbue_powers end, function(gem1, gem1_item)
-			player:showInventory("Use which second gem?", player:getInven("INVEN"), function(gem2) return gem2.type == "gem" and (gem2.material_level or 99) <= amulet.material_level and gem1.name ~= gem2.name and gem2.imbue_powers end, function(gem2, gem2_item)
+	player:showInventory("어떤 목걸이에 보석을 주입시키실 겁니까?", player:getInven("INVEN"), function(o) return o.type == "jewelry" and o.subtype == "amulet" and not o.egoed and not o.unique and not o.rare end, function(amulet, amulet_item)
+		player:showInventory("어떤 보석을 첫번째로 사용하실 겁니까?", player:getInven("INVEN"), function(gem1) return gem1.type == "gem" and (gem1.material_level or 99) <= amulet.material_level and gem1.imbue_powers end, function(gem1, gem1_item)
+			player:showInventory("두번쨰 보석은 무엇으로 사용하실건지?", player:getInven("INVEN"), function(gem2) return gem2.type == "gem" and (gem2.material_level or 99) <= amulet.material_level and gem1.name ~= gem2.name and gem2.imbue_powers end, function(gem2, gem2_item)
 				local price = 390
-				if price > player.money then require("engine.ui.Dialog"):simplePopup("Not enough money", "Limmir needs more gold for the magical plating.") return end
+				if price > player.money then require("engine.ui.Dialog"):simplePopup("돈이 부족하군요", "리미르는 마법 도금을 위해서는 더욱 많은 돈이 필요합니다..") return end
 
-				require("engine.ui.Dialog"):yesnoPopup("Imbue cost", "You need to use "..price.." gold for the plating, do you accept?", function(ret) if ret then
+				require("engine.ui.Dialog"):yesnoPopup("보석 주입 가격", "당신이 부탁한 도금을 하려면 골드가 "..price.." 정도는 있어야 합니다, 도금을 하실겁니까??", function(ret) if ret then
 					player:incMoney(-price)
 					local gem3, tries = nil, 10
 					while gem3 == nil and tries > 0 do gem3 = game.zone:makeEntity(game.level, "object", {type="gem"}, nil, true) tries = tries - 1 end
@@ -65,7 +65,7 @@ local artifact_imbue_amulet = function(npc, player)
 					amulet.name = "Limmir's Amulet of the Moon"
 					amulet.been_imbued = true
 					amulet.unique = util.uuid()
-					game.logPlayer(player, "%s creates: %s", npc.name:capitalize(), amulet:getName{do_colour=true, no_count=true})
+					game.logPlayer(player, "%s 가 목걸이에 마법 도금을 하여 만듬: %s", npc.name:capitalize(), amulet:getName{do_colour=true, no_count=true})
 				end end, "예", "아니오")
 			end)
 		end)
@@ -73,16 +73,16 @@ local artifact_imbue_amulet = function(npc, player)
 end
 
 newChat{ id="welcome",
-	text = [[Welcome, @playername@, to my shop.]],
+	text = [[제 상점에 오신걸 환영합니다, @playername@.]],
 	answers = {
-		{"Let me see your wares.", action=function(npc, player)
+		{"당신이 가진 물건을 보여주시죠.", action=function(npc, player)
 			npc.store:loadup(game.level, game.zone)
 			npc.store:interact(player)
 		end, cond=function(npc, player) return npc.store and true or false end},
-		{"I am looking for special jewelry.", jump="jewelry"},
-		{"So you can infuse amulets in this place?", jump="artifact_jewelry", cond=function(npc, player) return npc.can_craft and player:hasQuest("master-jeweler") and player:isQuestStatus("master-jeweler", engine.Quest.COMPLETED, "limmir-survived") end},
-		{"I have found this tome; it looked important.", jump="quest", cond=function(npc, player) return npc.can_quest and player:hasQuest("master-jeweler") and player:hasQuest("master-jeweler"):has_tome(player) end},
-		{"Sorry I have to go!"},
+		{"저는 좀 특수한 장신구들을 찾고 있습니다.", jump="jewelry"},
+		{"여기서 목걸이에 보석 주입을 하는 일을 한다고 들었는데 사실입니까?", jump="artifact_jewelry", cond=function(npc, player) return npc.can_craft and player:hasQuest("master-jeweler") and player:isQuestStatus("master-jeweler", engine.Quest.COMPLETED, "limmir-survived") end},
+		{"이런 큰 책을 찾았습니다만; 중요해 보이던데요.", jump="quest", cond=function(npc, player) return npc.can_quest and player:hasQuest("master-jeweler") and player:hasQuest("master-jeweler"):has_tome(player) end},
+		{"미안하지만 난 가봐야 겠어!"},
 	}
 }
 
@@ -91,8 +91,8 @@ newChat{ id="jewelry",
 If you bring me a gem and a non-magical ring, I can imbue the gem inside the ring for you.
 There is a small fee dependent on the level of the ring, and you need a quality ring to use a quality gem.]],
 	answers = {
-		{"I need your services.", action=imbue_ring},
-		{"Not now, thanks."},
+		{"그럼 어디 한번 해 봅시다.", action=imbue_ring},
+		{"고맙지만 지금은 됐네."},
 	}
 }
 
@@ -101,8 +101,8 @@ newChat{ id="artifact_jewelry",
 Bring me a non-magical amulet and two different gems and I will turn them into a powerful amulet.
 I will not make you pay a fee for it since you helped me so much, but I am afraid the ritual requires a gold plating. This should be equal to about 390 gold pieces.]],
 	answers = {
-		{"I need your services.", action=artifact_imbue_amulet},
-		{"Not now, thanks."},
+		{"그럼 어서 해 보죠.", action=artifact_imbue_amulet},
+		{"고맙지만 지금은 됐습니다."},
 	}
 }
 
@@ -113,7 +113,7 @@ There are rumours about a site of power in the southern mountains. Old legends t
 A lake formed in the crater of the crash. The water of this lake, soaked in intense Moonlight for eons, should be sufficient to forge powerful artifacts!
 Go to the lake and then summon me with this scroll. I will retire to study the tome, awaiting your summon.]],
 	answers = {
-		{"I will see if I can find it.", action=function(npc, player)
+		{"제가 찾을 수 있는지 한번 알아보죠.", action=function(npc, player)
 			game.level:removeEntity(npc)
 			player:hasQuest("master-jeweler"):remove_tome(player)
 			player:hasQuest("master-jeweler"):start_search(player)
