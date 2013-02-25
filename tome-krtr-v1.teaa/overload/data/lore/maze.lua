08dbf9565739d4cc61425fba0e38191f5c0938fe
gameatnew <gameatnew@gmail.com>
2013-02-25 04:23:29 +0000

    1차

--- tome-krtr-v1.teaa/overload/data/lore/maze.lua
+++ tome-krtr-v1.teaa/overload/data/lore/maze.lua
@@ -1,4 +1,4 @@
-﻿-- ToME - Tales of Maj'Eyal
+ -- ToME - Tales of Maj'Eyal
 -- Copyright (C) 2009, 2010, 2011, 2012 Nicolas Casalini
 --
 -- This program is free software: you can redistribute it and/or modify
@@ -25,13 +25,13 @@ newLore{
 	id = "maze-note-1",
 	category = "maze",
 	name = "diary (the maze)",
-	lore = [[Dear diary,
+	lore = [[일기장에게,
 
-Lessons are off this week as my tutor has fallen ill, so I've decided to sneak out and have a wander round the old mazed ruins nearby. I know I'll get in trouble if I'm caught, but as long as I'm back in a couple of days no one will notice... Besides, I get so bored cooped up in those mountains! I want some fun!
+  내 지도교사가 질병에 걸려 이번주 강의는 취소 되었어. 그래서 나는 근처 오래된 미궁 폐허로 숨어들어가 주위를 둘러보기로 했지. 만약 붙잡힌다면 곤란해질거라는걸 알고는 있지만 며칠 내로 돌아만 온다면 아무도 눈치채질 못할테니까. 어쨌거나 산 속에 처박혀 있는건 너무 지겹다고! 난 재밌는걸 원해!
 
-This is rather a dirty place though. I've come across a few bandits and snakes in here, but nothing to threaten a grade 3 mage like me.
+  여기는 꽤나 더러운 장소다. 여기서 몇몇 강도들과 뱀 몇마리를 만났지만 나같은 3 등급 마법사를 위협할만한건 아무것도 없지.
 
-I remember hearing that this labyrinth used to be a prison used by the halfling king Roupar during the Age of Dusk, and that with the lawlessness of the time captives were simply sent here to rot. Some say a magical curse infected the place and turned them into bull-like monsters that patrol the halls to this day. How exciting!!
+  황혼의 시대에 하플링의 왕 로우파르가 이 미궁을 감옥으로 사용했다는 말을 들은 기억이 난다. 포로들은 그저 이곳으로 보내지고 시간의 무자비함 앞에서 썩어들어갔다지. 몇몇 사람들은 미궁이 마법의 저주를 받아 포로들을 황소같이 생긴 괴물들로 변하게 해 오늘날 복도를 순찰하게 하고 있다는 이야기를 하곤 한다. 흥미롭군!!
 ]],
 }
 
@@ -40,30 +40,30 @@ newLore{
 	category = "maze",
 	name = "diary (the maze)",
 	bloodstains = 7,
-	lore = [[I'm having so much fun! Probability Travel is making this little trip a breeze. And you should have seen the look on that bandit's face when I came out one wall, disappeared through another, and came around behind him! Hee hee hee...
+  lore = [[진짜 재밌어! 마법 확률 이동 주문으로 이 소소한 여행이 식은 죽 먹기가 되버렸다. 내가 한쪽 벽에서 튀어나와 다른 벽으로 들어가고 다시 강도의 뒤로 나타났을때 강도의 그 얼굴을 꼭 봤어야 해! 히히히...
 
-I still remember Archmage Tarelion's lecture about the spell - "Probability effects can be employed for ease of use, but beware thee of relying on them. With ease of use comes ease of mind and a weakening of one's will and concentration. Soon one will find oneself in a situation of risk, bereft of normal judgement of danger, and low on the mental resources to save onself. Heed thee well." Bah, what tosh!!! How dumb does he really think I am?!
+  난 아직도 마도사 탈레리온의 주문에 대한 강의를 기억하고 있다. - "확률적 효과를 사용한다는 것은 쉬울수 있다. 그러나 확률적 효과에 여러분들이 의지하지 않도록 주의하도록. 쉽게 사용할 수 있다는 것은 마음에 쉽다는 생각을 끼치고 이는 곧 사용자의 의지와 집중력을 흐리게 할 수 있다는 말이다. 머지않아 누군가는 스스로가 위험한 상황에 빠져있다는 것을 발견할거다. 위험에 맞닥뜨려 판단력은 흐려지고 스스로를 구할 정신력도 바닥을 길테지. 이 충고를 잘 새겨듣도록." 참나, 그런 허튼소리를!!! 얼마나 멍청하면 내가 그럴 거라고 생각하는 걸까?
 
-Besides, I'm enjoying myself - I'm having an adventure!!
-
-I saw something! I don't know what it was... but it was big and shadowy! But when I tried chasing it I got lost... Um, maybe I just imagined it? No, I'm sure it must be something cool and exciting, I just have to keep exploring!]],
+  어쨌든간에 난 제대로 즐기고 있다고 - 진짜 모험을 하는 중이란 말씀이지!!
+  
+  뭔가를 봤어! 그게 뭔지는 모르겠지만 말이지... 하지만 엄청 크고 어두웠어! 하지만 쫓아가려고 했을 때 놓쳐버렸지.... 음, 그냥 내 상상이였던건가? 아냐, 분명히 뭔가 멋지고 흥미진진한 무언가가 있는게 분명해, 계속 살펴봐야겠어!]],
 }
 
 newLore{
 	id = "maze-note-trap",
 	category = "maze",
 	name = "the perfect killing device",
-	lore = [[I have now devised the perfect trap for the horned beast that walks these halls! Truly he cannot avoid this amazing contraption - the perfect blend of technical mastery and nature's lethal gifts. Ah, how I look forward to having that monster's head mounted on my walls - it shall be the pride of my collection!
+  lore = [[복도를 돌아다니는 뿔 달린 짐승을 위해 직접 고안한 함정을 만들었다! 장담컨데 절대로 이 기묘한 함정을 피해갈 순 없을껄 - 기술적 숙련도와 자연이 준 치명적인 선물의 완벽한 조합을 말야. 아, 내 벽에 걸릴 괴물의 머리를 생각해봐 - 내 컬렉션 중에서도 자랑이 될거야!
 
-The contraption is elegant and simple, though many months I have spent getting the formula perfect. There are two vials attached together - one containing finely ground hemlock, the other containing a carefully prepared zinc compound. When the vials are broken the materials react with the air and pump out an amazing cloud of poisonous vapour! The poison is supremely effective, killing within minutes. All I have to do is carefully hide the vials beneath a thin piece of slate and wait for my prey to step upon the trap - then POOF, it's dead!
+  공식을 완벽하게 하기 위해서 몇 개월이나 투자했기 때문에 이 기묘함은 우아함에도 단순하다. 함께 붙어 있는 두개의 약병이 있고 - 하나에는 곱게 갈린 독미라니가 들어있고 다른 하나에는 미리 조심스럽게 준비한 아연 분말이 들어있다. 병들이 깨지고 두 성분이 공기와 반응하면 놀라운 독의 구름를 뿜어낼 거다! 이 독은 끝내주게 효과적이고 뭘 죽이는데 몇분도 걸리지 않지. 내가 해야 하는 것들은 단지 약병들을 얇은 판자 밑에 숨겨두고 함정을 밟기를 기다리는 것 뿐이야. 그리고서는 콰직, 죽은거지! 
 
-I have prepared a great many vials to last me throughout the hunting season. By this time next year I will have a trophy collection to match the kings!
+  사냥을 하는 동안 날 버티게 해줄 끝내주는 약병들을 많이 준비두었다. 내년 이맘때쯤이면 왕이랑 필적할 정도의 전리품 콜렉션을 모을수 있을거야!
 
-I seem to have misplaced one though... I'm sure it must be close by.
+  그런데 하나를 잘못 설치한것 같다... 이 근처인게 분명한데.
 
 
-No, NO! I have - I --- acci--- pain, such pa--______
-
+  아냐, 안돼! 이 - 이건 - 사고 --- 아파, 너무 아 --_____
+  
 
 #{italic}#You find a dusty case filled with many small vials of powder. They seem serviceable.#{normal}#]],
 	on_learn = function(who)
