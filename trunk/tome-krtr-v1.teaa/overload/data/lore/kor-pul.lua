573d1b8e649ca1d174910d4fc473d949bc710d38
godo8848@gmail.com <godo8848@gmail.com@c92dd2d4-de0a-8354-a0ac-2da9ae4e0dc5>
2013-03-08 04:13:20 +0000

    lore 추가 번역

--- trunk/tome-krtr-v1.teaa/overload/data/lore/kor-pul.lua
+++ trunk/tome-krtr-v1.teaa/overload/data/lore/kor-pul.lua
@@ -28,10 +28,9 @@ newLore{
 	kr_display_name = "코르'풀에서 발견한 일지의 일부",
 	lore = [[그 빌어먹을 제메키스 자식! 빌어먹을 정신 나간 실험!! 대체 왜 나는 그 가장 어두웠던 밤에 실험 참가를 동의한거지?!
 
-아 그래, 땡전 한 푼 없어서 그랬지. 하..
+아 그래, 땡전 한 푼 없어서 그랬지. 하...
 
-아무리 그래도, 내가 입고 있던 옷이 전송되지 않았을 줄이야! 처음 보는 낯선 땅에서, 입에 담기도 민망한 내 모습을 발견했을 때의 부끄러움이란.. 고맙게도 한 친절한 농부가 로브 하나를 빌려주었고, 그를 위해 약간의 일을 해줘 조금이지만 돈도 구했다. 딱 식량 약간과 일지 한 권, 그리고 오래되고 낡은 검 하나 살 돈 말이지. 그래도 이 근처에 언데드로 가득 찬 폐허가 많다는 소리를 들었을 때, 나는 어떤 부름을 받은 듯한 느낌을 받았다! 그렇지 않고서야 이 위대한 태양의 기사 텔싸르가 이런 곳에 올 리가 없지 않은가? 이 알지 못할 서쪽 땅에서, 나의 힘을 증명할 기회가 온 것이다!
-]],
+아무리 그래도, 내가 입고 있던 옷이 전송되지 않았을 줄이야! 처음 보는 낯선 땅에서, 입에 담기도 민망한 내 모습을 발견했을 때의 부끄러움이란... 고맙게도 한 친절한 농부가 로브 하나를 빌려주었고, 그를 위해 약간의 일을 해줘 조금이지만 돈도 구했어. 딱 식량 약간과 일지 한 권, 그리고 오래되고 낡은 검 하나 살 돈 말이지. 그래도 이 근처에 언데드로 가득 찬 폐허가 많다는 소리를 들었을 때, 나는 어떤 부름을 받은 듯한 느낌을 받았다고! 그렇지 않고서야 이 위대한 태양의 기사 텔싸르가 이런 곳에 올 리가 없잖아? 이 알지 못할 서쪽 땅에서, 나의 힘을 증명할 기회가 왔다고!]],
 }
 
 newLore{
@@ -39,11 +38,12 @@ newLore{
 	category = "kor'pul",
 	name = "journal page (kor'pul)",
 	kr_display_name = "코르'풀에서 발견한 일지의 일부",
-	lore = [[Aha, I have found the accursed undead that plague this nefarious dungeon! The skeletal fool was thankfully no match for me! I suppose it helped that he had no arms...
+	lore = [[하! 이 사악한 던전을 오염시키는 저주받을 언데드들을 찾았어. 고맙게도, 이 바보같은 뼈다귀들은 내 상대가 되지 못했지! 뭐, 그녀석들 팔이 없다는 사실이 도움이 되긴 했어...
 
-I've found myself an old shield that in spite of a few dents seems serviceable enough. Some of these rats are BIG, and giving them a strong bash with the shield helps to stop their poisonous bites before I get my sword to their necks. I also found a few gems - I may have to hunt round for more. Not out of any personal greed of course, but my noble quest requires that I gather resources to defeat the great evils in this land and back home!
+조금 찌그러지긴 했지만, 그래도 쓸만한 방패를 하나 찾았어. 이곳의 쥐들은 *너무* 커서 독이 묻은 이빨로 나를 물까봐 걱정했었는데, 이제 그녀석들을 이 방패로 후려쳐서 기절시키고 나면 내 칼로 놈들의 목을 치기 훨씬 쉬워질 것 같아.
+그리고 보석들도 몇 개 찾았어 - 조금 더 보석을 찾아봐야 할지도. 물론 개인적인 욕심때문은 아니야. 거대한 악을 무찌르고 집으로 돌아간다는 내 숭고한 임무를 수행하기 위해서는, 물자를 모을 필요가 있다고.
 
-Diamonds are my favourite, so sparkly.]],
+아, 다이아몬드! 내가 제일 좋아하는 물건이지. 정말 영롱하군...]],
 }
 
 newLore{
@@ -51,12 +51,11 @@ newLore{
 	category = "kor'pul",
 	name = "journal page (kor'pul)",
 	kr_display_name = "코르'풀에서 발견한 일지의 일부",
-	lore = [[This place is infested! I've found a lot of skeletons now, and unfortunately most of them have borne a full set of limbs. However my holy quest cannot to be denied! Plus I got a really great sword off one of the blighters, I can chop anything up easily now!
+	lore = [[이곳은 완전히 오염된 곳이 분명해! 이제 해골들이 정말 많이 보여. 그리고 불행하게도, 이제 대부분 팔다리가 멀쩡하게 붙어있지. 하지만 나의 신성한 임무를 멈출 수는 없어! 게다가 녀석들중 하나로부터 엄청 큰 칼을 빼앗았다고. 이제 뭐든지 썰어버릴 수 있어!
 
-The skeletal mages have been a night-born nuisance, but I've found a new weapon to use against them - a phase door rune! As soon as I catch sight of one of the robed wretches I activate my rune and foom, I'm away!
+해골 마법사는 정말 귀찮은 어둠의 자식이야. 하지만 그녀석들을 상대할 새로운 무기를 찾았지 - 근거리 순간이동 룬! 저 로브를 입은 개자식들 중 하나의 시선이 느껴지면, 룬을 발동시켜서 뿅! 사라지는거지!
 
-It's not fleeing, it's just tactical repositioning...
-]],
+...이건 도망가는게 아니야. 전략적인 위치 재설정 과정이라고.]],
 }
 
 newLore{
@@ -64,9 +63,12 @@ newLore{
 	category = "kor'pul",
 	name = "journal page (kor'pul)",
 	kr_display_name = "코르'풀에서 발견한 일지의 일부",
-	lore = [[Kor'Pul, Kor'Pul... When the farmer told me what this place was called it reminded me of something, and I think it's coming back to me now. My mother used to tell me a story about our ancestors, how they fled by ship to escape the grasp of an evil sorcerer who dominated the lands. The sorcerer was a vile necromancer who took advantage of the destruction from the Spellblaze and the Cataclysm to create huge armies of undead. The people fought against him time and time again, but though he would be defeated he would still come back, sometimes after hundreds of years. And that sorcerer's name was.... Kor'Pul.
+	lore = [[코르'풀, 코르'풀... 농부가 이곳의 이름을 말해줬을 때 뭔가 생각날려고 했는데, 지금 딱 떠오른거 같아.
 
-Probably just a coincidence.]],
+어머니는 세상을 지배하고 있던 악의 마법사의 손아귀로부터 도망쳤다는, 우리 조상들에 대한 이야기를 해주시곤 했지. 그 마법사는 마법폭발이 일으킨 파괴와 대재앙을 이용해, 거대한 언데드 군대를 만들어낸 사악한 사령술사라고 했어.
+사람들은 계속해서 그와 싸웠고, 결국 그를 물리쳤지. 하지만 사람들이 말하길, 그는 언젠가 다시 돌아올지도 모른다고 했어. 몇백 년의 시간이 흐른 후에라도 말야. 그 사악한 마법사의 이름이 코르'풀... 이었어.
+
+뭐, 우연의 일치겠지...]],
 }
 
 newLore{
@@ -74,9 +76,13 @@ newLore{
 	category = "kor'pul",
 	name = "journal page (kor'pul)",
 	kr_display_name = "코르'풀에서 발견한 일지의 일부",
-	lore = [[It's quiet down here. And dark... very dark. I suppose I should have brought a lantern. Our motto is to bring light into dark places, but I guess I should have thought about that in practical terms. I've cleared out pretty much all of this area, and there's not much left to explore beyond this last room.
+	lore = [[이제 꽤 많이 내려온 것 같아. 어두워... 정말 어두워. 랜턴을 가져왔어야 하는건데. '우리는 어두운 곳에 빛을 가져온다' 는 우리의 행동양식을 조금 더 현실적으로 받아들일걸...
 
-This adventuring stuff is quite lonely, I must say. I guess keeping this journal helps a bit. I miss home a lot now. I miss... I miss Falia. Maybe I should have said something to her about how I feel... but I guess running away on an adventure seemed easier. Pah, how brave of me...
+아무튼 이 지역의 대부분은 정리가 끝난 것 같아. 이제 여기 있는 마지막 방만이 남았다고 할 수 있지.
 
-I'm not cut out for this paladin work. Here I am, alone in a horrible dark dungeon in an unknown land, without a single sound to keep me company beyond the scratchings of my own quill. Hmm, except for--]],
+꽤나 쓸쓸한 모험이였어... 이 일지를 적는게 쓸쓸함을 덜어주긴 했지만, 이제 정말 집이 그리워... 그리워... 팔리아를 보고 싶어.
+그녀에게 내 마음에 대해 이야기를 했어야 했는데... 하지만 모험한답시고 이렇게 도망가는게 그녀에게 고백하는 것보다 더 쉬워보였지... 핫, 난 정말 용감하군 그래...
+
+그래도 태양의 기사 일을 그만두지는 않겠어. 이 알지도 못하는 땅의 끔찍하고 어두운 던전 안에 홀로 있는 처지지만 말야. 휴, 정말 내 깃펜이 스치는 소리 외에는 아무 것도 들리지 않는군 그래. 
+잠깐, 방금 무슨 소리가 났--]],
 }
--- trunk/tome-krtr-v1.teaa/overload/data/lore/old-forest.lua
+++ trunk/tome-krtr-v1.teaa/overload/data/lore/old-forest.lua
@@ -26,14 +26,14 @@ newLore{
 	category = "old forest",
 	name = "journal entry (old forest)",
 	kr_display_name = "오래된 숲에서 발견한 일지",
-	lore = [[#{italic}#From the notes of Darwood Oakton, explorer:
-#{bold}#CHAPTER ONE: THE SHER'TUL
+	lore = [[#{italic}#탐험가 다르우드 오크톤의 쪽지에서 발췌 :
+#{bold}#첫 번째 이야기 : 쉐르'툴 종족
 
-#{normal}#The Sher'Tul. Who were they? Where did they come from? Where did they go? The mysteries surrounding this ancient race are almost infinite. What little scraps of information we have regarding them allude to a mighty and world-spanning civilisation, wielding power and magic unthinkable. Now, however, all that remains of them are forgotten, wind-swept ruins, the tiniest minutiae of their technology sealed away in the studies of reclusive sages. Does their mystery not call to your curious nature as it does mine, gentle reader?
+#{normal}#쉐르'툴. 그들은 누구였는가? 그들은 어디에서 왔는가? 그들은 어디로 갔는가? 이 고대의 종족을 둘러싼 의문점들은 무한히 많습니다. 그들에 대해 암시된 정보의 편린들을 모아보면 그들은 강력하며, 세계 전역에 걸친 문명과 도시를 가지고 있었고, 그들의 힘과 마법은 상상을 초월할 수준이었다고 합니다. 하지만, 오늘날 그들이 남긴 모든 것들은 잊혀지고, 풍화된 유적이 되었으며, 그들의 가장 사소한 기술마저도 봉인되고 사라져 은둔한 현자들의 연구 대상이 되고 말았습니다. 이 글을 읽고 계신 친절한 독자 여러분, 그들의 신비에 대해 제가 가지고 있는 것 만큼의 자연스러운 호기심이 생겨나지 않습니까?
 
-My quest has drawn me into the Old Forest. What is there to be said about a place like "the old forest"? It is a forest, and it is old. By its unimaginative moniker you can guess how important this place is to the people of Derth; the only locals who commonly venture under its boughs are novice alchemists in search of ingredients, plus the odd hunter with his sights set low. However, the story of this old forest now takes a more interesting twist...
+제 임무를 위해, 저는 이곳 오래된 숲에 왔습니다. 어째서 이곳이 "오래된 숲" 으로 불리는지 아십니까? 물론 이곳이 숲이며, 오래되었기 때문입니다. 이 따분하고 상상력 부족한 이름 때문에, 데르스 사람들이 얼마나 이곳을 "중요하게" 여기는지 잘 알 수 있습니다. 그렇습니다, 이곳은 초보 연금술사들이 재료를 찾기 위해 나뭇가지를 꺾어가는 곳, 아니면 별로 대단한 사냥감을 바라지 않는 이상한 사냥꾼이나 오는 곳으로 여겨집니다. 하지만 이 오래된 숲에는 조금 더 재미있는 이야기가 전해져 내려옵니다...
 
-Rumours are growing of trees roaming in its depths, moving as you or I would. Some even claim that they now possess the spark of sentience. The Sher'Tul were rumoured to hold the power of animism... is this mere coincidence?]],
+소문에 의하면, 오래된 숲의 깊은 곳에서는 스스로의 의지로 움직일 수 있는 나무들이 자라고 있다고 합니다. 몇몇 사람들은 이 나무들이 이성을 가지고 있다고 주장하기도 합니다. 그리고, 쉐르'툴 종족들은 동물이나 나무를 조종할 수 있는 힘을 가지고 있었다는 소문이 있습니다... 과연 이것이 단순한 우연의 일치로 생겨난 일일까요?]],
 }
 
 newLore{
@@ -41,10 +41,10 @@ newLore{
 	category = "old forest",
 	name = "journal entry (old forest)",
 	kr_display_name = "오래된 숲에서 발견한 일지",
-	lore = [[#{italic}#From the notes of Darwood Oakton, explorer:
-#{bold}#CHAPTER TWO: ANCIENT RUINS
+	lore = [[#{italic}#탐험가 다르우드 오크톤의 쪽지에서 발췌 :
+#{bold}#두 번째 이야기 : 고대의 유적
 
-#{normal}#My inquiries have paid off! It took much searching, and even more arm-twisting and cajoling once I had found my man, but a local lumberjack who plies his trade in the old forest has divulged to me an amazing secret! He speaks of ruins within the forest, a location where the living trees seem to congregate in larger numbers. He would not speak much of the place, and seemed to believe it cursed, but I did manage to squeeze out of him the appearance of the ruins, submerged in the middle of the great lake. There is no longer any doubt in my mind now: They belonged to the Sher'Tul!]],
+#{normal}#제 탐험이 드디어 결실을 맺었습니다! 탐색과 강압과 회유를 반복한 끝에, 이 오래된 숲에 숨겨진 놀라운 비밀을 알려줄, 오래된 숲 근처에 사는 나무꾼을 찾은 것입니다! 그는 살아 움직이는 나무들이 많이 모인 곳이자 오래된 숲 깊은 곳에, 어떤 유적이 있다고 말했습니다. 그는 그 장소에 대해 그리 많은 말을 하지 않았으며, 그 장소를 저주받았다고 믿는 눈치였습니다. 하지만 저는 그 장소의 위치에 대해 더 캐물었고, 그 결과 거대한 호수 밑에 유적이 가라앉아 있다는 정보를 얻었습니다. 이제 더 의심할 여지가 없습니다. 그곳은 쉐르'툴 유적이 분명합니다!]],
 }
 
 newLore{
@@ -52,10 +52,10 @@ newLore{
 	category = "old forest",
 	name = "journal entry (old forest)",
 	kr_display_name = "오래된 숲에서 발견한 일지",
-	lore = [[#{italic}#From the notes of Darwood Oakton, explorer:
-#{bold}#CHAPTER THREE: DISASTER!
+	lore = [[#{italic}#탐험가 다르우드 오크톤의 쪽지에서 발췌 :
+#{bold}#세 번째 이야기 : 재앙!
 
-#{normal}#Does my title not tell you enough? Disaster, and again disaster! True enough, these Sher'Tul ruins exist... several hundred feet at the bottom of a mighty lake! The lake of Nur, one of the largest in the old forest, has swallowed up the ruins in its murky depths. I am hardly a strong swimmer, gentle reader, but even if I could swim like a naga-spawned beast I could not hope to explore the ruin's sunken expanses before drowning. I fear I must abandon my present expedition... the trees are paying closer attention to me, and I do not believe it is of the pleasant sort...]],
+#{normal}#제목에 써진 단어로 충분하지 않습니까? 재앙, 재앙입니다! 정말로, 쉐르'툴 유적은 있었습니다... 거대한 호수의 밑바닥, 수십 미터 깊이의 호수 밑바닥에 말이죠! 오래된 숲에 있는 가장 큰 지형 중 하나인 누르 호수가, 그 어두컴컴하고 깊은 바닥 아래로 유적을 집어삼킨 것입니다. 친절한 독자 분들이시여, 비록 제가 그리 수영을 잘 하지는 못하지만, 나가 같은 짐승이 온다고 해도 이 유적을 탐사하기 전에 숨이 막혀 죽을 것이 분명합니다. 아, 지금 하고 있는 탐험이 중지될까 겁이 납니다... 그 나무들이 저에게 관심을 쏟고 있다는게 느껴집니다. 그리 친절한 종류의 관심은 아닌 것 같습니다...]],
 }
 
 newLore{
@@ -63,10 +63,10 @@ newLore{
 	category = "old forest",
 	name = "journal entry (old forest)",
 	kr_display_name = "오래된 숲에서 발견한 일지",
-	lore = [[#{italic}#From the notes of Darwood Oakton, explorer:
-#{bold}#CHAPTER FOUR: NEEDS MUST...
+	lore = [[#{italic}#탐험가 다르우드 오크톤의 쪽지에서 발췌 :
+#{bold}#네 번째 이야기 : 반드시 필요한 것은...
 
-#{normal}#Before I continue, I must make one thing clear: I am no great friend to the mages. Some powers simply were not meant for mortal hands or minds. As history has taught us time and again, from the sudden disappearance of the Sher'Tul to the Spellblaze and the plagues it brought in its wake, magic is wont to cause more harm than good. But I fear it is a necessity for my current task. During my stay in Derth a fellow traveller and I have become fast friends, often drinking together in the local tavern. I can't put my finger on it, but I believe him to be a mage; he has an unexplainable feeling of power surrounding him, not to mention a rather ostentatious hat. I wonder what his thoughts would be on the art of water-breathing...?]],
+#{normal}#이 탐험을 계속하기 전에, 한 가지 사실을 확실히 해야겠군요. 저는 절친한 마법사 친구가 하나도 없습니다. 그 힘은 필멸자의 손이나 정신으로 다루어서는 안되는 힘입니다. 쉐르'툴 종족이 갑자기 사라진 것부터 마법폭발과 그것이 불러온 질병들까지, 마법은 좋은 일보다는 해를 끼치는 경우가 훨씬 더 많다는 사실을 역사는 우리에게 끊임없이 말해주고 있기 때문입니다. 하지만 지금 제 일을 하기 위해서는 마법이 필요하다는 사실이 두렵습니다. 그러고보니 제가 데르스에 머무르는 동안, 한 길동무를 만나 빠르게 친구가 된 적이 있습니다. 그곳의 여관에서 함께 술을 마시는 동안, 정확히 기억은 안 나지만, 그가 마법사라는 말을 들었던 것 같습니다. 그의 주위에서는 설명할 수 없는 종류의 힘이 느껴졌습니다. 대단히 비싸보이는 모자를 쓰고 있었다는 것은 말할 필요도 없지요. 아마 그라면 물 속에서 숨을 쉴 수 있게 해주는 기술을 알고 있을지도...?]],
 }
 
 newLore{
@@ -75,26 +75,25 @@ newLore{
 	name = "journal entry (old forest)",
 	kr_display_name = "오래된 숲에서 발견한 일지",
 	bloodstains = 12,
-	lore = [[#{italic}#From the notes of Darwood Oakton, explorer:
-#{bold}#CHAPTER FIVE: HORR...
+	lore = [[#{italic}#탐험가 다르우드 오크톤의 쪽지에서 발췌 :
+#{bold}#다섯 번째 이야기 : 공ㅍ...
 
-#{italic}#This note seems hastily written and stained with water and blood.
+#{italic}#이 쪽지는 급하게 써진 것으로 보이며, 물과 피로 범벅이 된 상태이다.
 
-#{normal}#I... I haven't got long.
-The key is here, but I never ... the door.
+#{normal}#저... 저는 틀리지 않았습니다.
+열쇠는 여기에 있지만, 저는 결코 그 문을 ... 않았어야 했습니다.
 
-I was unprepared! The trees I could avoid, the water I ... traverse, but beyond...
-Horrors ... tentacles ... blazing light that burned in an instant ...
-My flesh devoured, my mind shattered ... worms, alive, walking tog ...
-... barely escaped. But my wounds are too ... blood won't stop ...
+준비가 부족했습니다! 그 나무들은 피할 수 있었고, 그 물 속은 지나올 수 있었지만...
+공포들 ... 촉수들 ... 몸을 순식간에 불태우던 그 타오르는 빛 ...
+제 육체는 잡아먹히고, 제 정신은 산산조각 ... 벌레들, 살아있는, 걸어다니는 ...
+... 간신히 도망쳤습니다. 하지만 제 상처는 너무나 ... 피가 멈추지 않 ...
 
-I thought the Sher'Tul wonderful, I was entranced ... every facet of knowledge I could gain ...
-But this, is this their legacy? ... horrifying ... all my dreams ... Perhaps death is welcome now...
+쉐르'툴 종족은 그저 아름답고 경이로울 것이라고 여겼지만, 제가 들어가서 본 ... 얻을 수 있었던 모든 지식은 그런 측면을 ...
+하지만 이런, 이런 것이 그들의 유산이라고? ... 끔찍합니다 ... 제 모든 꿈들이 ... 차라리 죽음을 반기는 것이 좋을지도 ...
 
-If any come after, I bid you turn ... horrors ... too much. If you are foolish enough to ... my only advice is to ...
+이곳에 발을 내딛는 또 다른 자가 있다면, 부디 도망 ... 공포들 ... 견딜 수가 없습니다. 만약 당신이 저처럼 어리석은 자라면 ... 제 유일한 충고는 ...
 
-#{italic}#You find with the note a tiny, faintly glowing orb - is this the key the note mentions?
-#{normal}#]],
+#{italic}#쪽지와 함께 있던, 아주 작고 희미하게 빛나는 오브를 발견하였다. 아마 이것이 쪽지에서 언급된 '열쇠' 일까?#{normal}#]],
 	on_learn = function(who)
 		game.player:grantQuest("shertul-fortress")
 	end,
