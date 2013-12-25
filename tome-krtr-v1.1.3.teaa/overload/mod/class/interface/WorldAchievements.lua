888a10bda6df1303d781a1a2c295c614be5dd381
jinhlk <jinhlk@gmail.com>
2013-12-25 05:38:31 +0000

    v1.1.3으로 기존 한글화 이동했습니다. (v1.1.2 대응 코드는 삭제했습니다) 관련 내용은 issue23 을 참조해 주십시오.
    기존 한글화 주석 중 시스템이나 코드 변경 참조 사항은 --@로 변경했습니다 (이 주석은 왠만하면 삭제하지 말아주세요..)
    한글화가 필요한 부분은 여전히 --@@로 달려있습니다.

---
+++ tome-krtr-v1.1.3.teaa/overload/mod/class/interface/WorldAchievements.lua
@@ -0,0 +1,143 @@
+﻿-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010, 2011, 2012, 2013 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+require "engine.class"
+local WA = require "engine.interface.WorldAchievements"
+
+--- Handles achievements in a world
+module(..., package.seeall, class.inherit(class.make{}, WA))
+
+-- Duplicate from Game.lua because Game.lua is not loaded yet
+local DIFFICULTY_EASY = 1
+local DIFFICULTY_NORMAL = 2
+local DIFFICULTY_NIGHTMARE = 3
+local DIFFICULTY_INSANE = 4
+local DIFFICULTY_MADNESS = 5
+local PERMADEATH_INFINITE = 1
+local PERMADEATH_MANY = 2
+local PERMADEATH_ONE = 3
+
+local function findTile(t)
+	if not fs.exists("/data/gfx/achievements/"..t.id:lower()..".png") then
+		t.image = "trophy_gold.png"
+		print("Achievement with default image not found", t.id, "/data/gfx/achievements/"..t.id:lower()..".png")
+	else t.image = "achievements/"..t.id:lower()..".png"
+	end
+end
+
+--- Make a new achievement with a name and desc
+function _M:newAchievement(t)
+	t.id = t.id or t.name
+	t.id = t.id:upper():gsub("[ ]", "_")
+	findTile(t)
+
+	WA.newAchievement(self, t)
+
+	if not t.no_difficulty_duplicate then
+		-- Normal
+		local t2 = table.clone(t)
+		t2.id = "NORMAL_ROGUELIKE_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (로그라이크)"
+		t2.name = t2.name.." (Roguelike)"
+		t2.difficulty = DIFFICULTY_NORMAL
+		t2.permadeath = PERMADEATH_ONE
+		findTile(t2)
+		WA.newAchievement(self, t2)
+		
+		-- Exploration
+		local t2 = table.clone(t)
+		t2.id = "EXPLORATION_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (탐사 모드)"
+		t2.name = t2.name.." (Exploration mode)"
+		t2.permadeath = PERMADEATH_INFINITE
+		findTile(t2)
+		WA.newAchievement(self, t2)
+
+		-- Nightmare
+		local t2 = table.clone(t)
+		t2.id = "NIGHTMARE_ADVENTURE_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (악몽 (모험) 난이도)"
+		t2.name = t2.name.." (Nightmare (Adventure) difficulty)"
+		t2.difficulty = DIFFICULTY_NIGHTMARE
+		t2.permadeath = PERMADEATH_MANY
+		findTile(t2)
+		WA.newAchievement(self, t2)
+
+		local t2 = table.clone(t)
+		t2.id = "NIGHTMARE_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (악몽 (로그라이크) 난이도)"
+		t2.name = t2.name.." (Nightmare (Roguelike) difficulty)"
+		t2.difficulty = DIFFICULTY_NIGHTMARE
+		t2.permadeath = PERMADEATH_ONE
+		findTile(t2)
+		WA.newAchievement(self, t2)
+
+		-- Insane
+		local t2 = table.clone(t)
+		t2.id = "INSANE_ADVENTURE_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (정신나간 (모험) 난이도)"
+		t2.name = t2.name.." (Insane (Adventure) difficulty)"
+		t2.difficulty = DIFFICULTY_INSANE
+		t2.permadeath = PERMADEATH_MANY
+		findTile(t2)
+		WA.newAchievement(self, t2)
+
+		local t2 = table.clone(t)
+		t2.id = "INSANE_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (정신나간 (로그라이크) 난이도)"
+		t2.name = t2.name.." (Insane (Roguelike) difficulty)"
+		t2.difficulty = DIFFICULTY_INSANE
+		t2.permadeath = PERMADEATH_ONE
+		findTile(t2)
+		WA.newAchievement(self, t2)
+		
+		-- Madness
+		local t2 = table.clone(t)
+		t2.id = "MADNESS_ADVENTURE_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (미치광이 (모험) 난이도)"
+		t2.name = t2.name.." (Madness (Adventure) difficulty)"
+		t2.difficulty = DIFFICULTY_MADNESS
+		t2.permadeath = PERMADEATH_MANY
+		findTile(t2)
+		WA.newAchievement(self, t2)
+
+		local t2 = table.clone(t)
+		t2.id = "MADNESS_"..t2.id
+		t2.kr_name = (t2.kr_name or t2.name).." (미치광이 (로그라이크) 난이도)"
+		t2.name = t2.name.." (Madness (Roguelike) difficulty)"
+		t2.difficulty = DIFFICULTY_MADNESS
+		t2.permadeath = PERMADEATH_ONE
+		findTile(t2)
+		WA.newAchievement(self, t2)
+	end
+end
+
+function _M:gainAchievement(id, src, ...)
+	-- Redirect achievements to the main player, always
+	src = game.party:findMember{main=true}
+	local ret = WA.gainAchievement(self, id, src, ...)
+
+	if ret then
+		game:onTickEnd(function() game:playSound("actions/achievement") end, "achievementsound")
+		game.state:checkDonation(true) -- They gained someting nice, they could be more receptive
+
+		if core.steam and not config.settings.cheat then core.steam.forwardAchievement("TOME_"..id) end
+	end
+	return ret
+end
