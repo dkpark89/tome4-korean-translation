1fbb5a4e0429ba312ebe5be04bfc984b4eea679e
jinhlk@gmail.com <jinhlk@gmail.com@c92dd2d4-de0a-8354-a0ac-2da9ae4e0dc5>
2014-07-13 02:01:15 +0000

    애드온 등록을 위해 init.lua 파일을 수정했습니다.

--- trunk/tome-krtr-v1.2.3.teaa/init.lua
+++ trunk/tome-krtr-v1.2.3.teaa/init.lua
@@ -18,17 +18,26 @@
 -- darkgod@te4.org
 
 long_name = "Korean translation addon"
-short_name = "krTr"
+short_name = "koreanTr"
 for_module = "tome"
 version = {1,2,3}
 
 weight = 10 -- 한글화가 먼저되고 다른 애드온이 실행되는게 애드온끼리의 충돌이 적을거 같아 무게를 가볍게 잡음 (100이 제일 나중인 듯)
 tags = {'Korean','translation'}
 author = { "nethackboard", "blank@blank.com" }
-homepage = "http://tome.te4.org/"
+homepage = "http://nethack.byus.net/"
 description = [[ToME4 korean translation add-on by nethack board
 
-우리말화(한글화) 애드온입니다!]]
+This is Korean Translation addon.
+Many system(engine/mod) files are overloaded.
+So can be conflict with other addon which modify system files.
+And DO NOT USE when version is different.
+
+우리말화(한글화) 애드온입니다!
+이 애드온은 많은 시스템(engine/mod) 파일들을 덮어 씌웁니다 (현재 번역을 위해서는 이렇게 할 수 밖에 없습니다).
+따라서 시스템 파일을 수정하는 다른 애드온과는 충돌이 발생할 확률이 높습니다. (대부분의 시스템 파일을 수정하지 않고 데이타만 추가하는 애드온과는 문제가 없습니다)
+그리고, 버전이 다른 경우에는 사용하지 마십시오 (한글 애드온 버전 이후의 변경사항이 다시 덮어씌워져 에러가 날 수도 있고, 에러가 나지 않는다 해도 달라진 점이 적용되지 않는 경우가 많습니다). 
+]]
 
 overload = true
 superload = true
