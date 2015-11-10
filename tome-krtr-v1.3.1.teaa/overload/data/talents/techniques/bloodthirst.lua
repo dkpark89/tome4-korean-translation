2ec6d0a0cedf00b9e8f893ba517bcc41f2b3a169
Jolgame <jolgame@gmail.com>
2015-11-10 23:04:48 +0900

    Update bloodthirst.lua
    1.2.3 업뎃

--- tome-krtr-v1.3.1.teaa/overload/data/talents/techniques/bloodthirst.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/talents/techniques/bloodthirst.lua
@@ -19,6 +19,7 @@
 
 newTalent{
 	name = "Mortal Terror",
+	kr_name = "극심한 공포",
 	type = {"technique/bloodthirst", 1},
 	require = techs_req_high1,
 	points = 5,
@@ -37,22 +38,23 @@ newTalent{
 		if target:canBe("stun") then
 			target:setEffect(target.EFF_DAZED, 5, {apply_power=self:combatPhysicalpower()})
 		else
-			game.logSeen(target, "%s resists the terror!", target.name:capitalize())
+			game.logSeen(target, "%s가 죽음의 공포에 저항했습니다!", target.name:capitalize())
 		end
 	end,
 	passives = function(self, t, p)
 		self:talentTemporaryValue(p, "combat_physcrit", t.getCrit(self, t))
 	end,
 	info = function(self, t)
-		return ([[Your mighty blows inspire utter terror on your foes. Any melee strike you do that deals more than %d%% of the target's total life puts them in a mortal terror, dazing them for 5 turns.
-		Your critical strike chance also increase by %d%%.
-		The daze chance increase with your Physical Power.]]):
+		return ([[강력한 공격으로 적에게 죽음에 대한 공포를 심어줍니다. 
+		한 번의 근접공격으로 대상이 지닌 최대 생명력의 %d%% 가 넘는 피해를 입히면, 대상은 죽음의 공포에 빠져 5 턴 동안 혼절합니다.
+		치명타율이 %d%% 증가하는 효과도 있으며, 혼절 확률은 물리력의 영향을 받아 증가합니다.]]):
 		format(t.threshold(self, t), self:getTalentLevelRaw(t) * 2.8)
 	end,
 }
 
 newTalent{
 	name = "Bloodbath",
+	kr_name = "피바다",
 	type = {"technique/bloodthirst", 2},
 	require = techs_req_high2,
 	points = 5,
@@ -69,14 +71,15 @@ newTalent{
 		local regen = t.getRegen(self, t)
 		local max_regen = t.getMax(self, t)
 		local max_health = t.getHealth(self,t)
-		return ([[Delight in spilling the blood of your foes.  After scoring a critical hit, your maximum hit points will be increased by %d%%, your life regeneration by %0.2f per turn, and your stamina regeneration by %0.2f per turn for %d turns.
-		The life and stamina regeneration will stack up to five times, for a maximum of %0.2f and %0.2f each turn, respectively.]]):
+		return ([[적들이 흘리는 피를 보고 희열을 느낍니다. 적에게 치명타를 가하면 %d 턴 동안 최대 생명력이 %d%% 상승하며, 턴 당 생명력 재생이 %0.2f / 턴 당 체력 재생이 %0.2f 증가합니다.
+		생명력과 체력 재생 증가 효과는 5 회까지 중첩 가능합니다. 즉, 턴 당 생명력 재생은 최대 %0.2f / 턴 당 체력 재생은 최대 %0.2f 까지 가능합니다.]]):
 		format(t.getHealth(self, t), regen, regen/5, t.getDuration(self, t),max_regen, max_regen/5)
 	end,
 }
 
 newTalent{
 	name = "Bloody Butcher",
+	kr_name = "핏빛 도살자",
 	type = {"technique/bloodthirst", 3},
 	require = techs_req_high3,
 	points = 5,
@@ -84,15 +87,16 @@ newTalent{
 	getDam = function(self, t) return self:combatScale(self:getStr(5, true) * self:getTalentLevel(t), 5, 0, 40, 35) end,
 	getResist = function(self,t) return self:combatTalentScale(t, 10, 40) end,
 	info = function(self, t)
-		return ([[You delight in the inflicting of wounds, providing %d physical power.
-		In addition when you make a creature bleed its physical damage resistance is reduced by %d%% (but never below 0%%).
-		Physical power depends on your Strength stat.]]):
+		return ([[적에게 상처내는 것을 즐기게 되어, 물리력이 %d 상승합니다.
+		또한 적이 출혈 상태에 빠질 경우, 적의 물리 저항력 역시 %d%% 감소하게 됩니다. (단, 저항력은 0%% 아래로 내려가지 않습니다)
+		물리력 상승량은 힘 능력치의 영향을 받아 증가합니다.]]):
 		format(t.getDam(self, t), t.getResist(self, t))
 	end,
 }
 
 newTalent{
 	name = "Unstoppable",
+	kr_name = "무쌍",
 	type = {"technique/bloodthirst", 4},
 	require = techs_req_high4,
 	points = 5,
@@ -107,9 +111,9 @@ newTalent{
 		return true
 	end,
 	info = function(self, t)
-		return ([[You enter a battle frenzy for %d turns. During that time, you can not use items, healing has no effect, and your health cannot drop below 1.
-		At the end of the frenzy, you regain %d%% of your health per foe slain during the frenzy.
-		While Unstoppable is active, Berserker Rage critical bonus is disabled as you lose the thrill of the risk of death.]]):
+		return ([[%d 턴 동안 전투 광란 상태가 됩니다. 효과가 지속되는 동안에는 물품을 사용할 수 없고 치유 효과도 적용되지 않지만, 생명력이 1 밑으로 떨어지지 않습니다.
+		전투 광란 상태가 끝나면, 전투 광란 상태에서 살해한 적 하나당 전체 생명력의 %d%% 에 해당하는 생명력을 회복합니다.
+		무쌍 기술이 활성화 중인 동안, '광전사의 분노' 기술의 치명타 증가량은 적용되지 않습니다. 죽음에 대한 공포와 그로 인한 분노가 사라져버리기 때문입니다.]]):
 		format(t.getDuration(self, t), t.getHealPercent(self,t))
 	end,
 }
