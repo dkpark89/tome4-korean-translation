dbcd43c88df33b83ffef5a10642f4961e00cfa53
Jolgame <jolgame@gmail.com>
2015-11-13 11:54:50 +0900

    Update calc11.lua

--- tome-krtr-v1.3.1.teaa/overload/data/texts/tutorial/stats-calc/calc11.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/texts/tutorial/stats-calc/calc11.lua
@@ -18,5 +18,11 @@
 -- darkgod@te4.org
 
 return [[
-Descend to the penultimate level of the #GOLD#Dungeon of Adventurer Enlightenment#WHITE# to learn all about detrimental #GOLD#timed effects.#WHITE#
+사실, 예전에는 수치 차이가 1 날 때마다 공격의 성공/실패 확률이 10% 씩 왔다갔다 했었습니다. 이번 층계의 미궁 구성도 이 때의 공식을 바탕으로 만들어진 것이죠.
+
+수치 차이가 10 만 나도 무조건 실패/성공이 가능하다보니, 유저들 사이에서 불만이 많았나봅니다. 그래서 공식이 지금처럼 바뀌게 되었는데, 이 연습용 게임은 예전의 공식을 그대로 적용 중이네요. :(
+
+이번 층계에서 지금까지 본 텍스트들은 죄다 제가 새로 만들어낸 부분입니다. 저도 한글화 하면서 전투 공식이 바뀐 부분은 처음 알았네요. 제작자는 각성하라 *_*!!
+
+뭐 잡설은 이정도로 줄이고.. #GOLD#모든 초보 모험가들을 위한 계몽의 지하미궁#WHITE# 의 아래층으로 내려가세요. 다음 층계에서는 #GOLD#지속 효과#WHITE# 에 대한 내용을 다룹니다.
 ]]
