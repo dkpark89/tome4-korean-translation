c3858cff08c264bc1fc62303d151cf24742f7929
Jolgame <jolgame@gmail.com>
2015-11-13 11:55:18 +0900

    Update calc2.lua

--- tome-krtr-v1.3.1.teaa/overload/data/texts/tutorial/stats-calc/calc2.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/texts/tutorial/stats-calc/calc2.lua
@@ -18,7 +18,7 @@
 -- darkgod@te4.org
 
 return [[
-If you were wearing your #GOLD#Helmet of Accuracy (+6)#WHITE#, then you should have #LIGHT_GREEN#38 Accuracy#WHITE#, while the orc has #LIGHT_GREEN#48 Defense#WHITE#. The difference is ten in the orc's favor, which is apparently too much for you to be able to hit him.
+제 충고를 받아들여 #GOLD#정확도의 투구 (+6)#WHITE# 를 장비했다면, #LIGHT_GREEN#43 정확도#WHITE# 를 가지고 있었겠네요. 방금 전의 오크는 #LIGHT_GREEN#48 회피도#WHITE# 를 가지고 있었습니다. 수치가 5 만큼 차이가 나네요. 흠...
 
-There's another orc ahead with slightly lower defense. Go get him!
+앞에 있는 오크는 회피도 수치가 조금 낮은 편입니다. 싸워보세요!
 ]]
