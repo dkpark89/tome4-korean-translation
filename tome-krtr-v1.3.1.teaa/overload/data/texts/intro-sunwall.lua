602671670e28bd71f3856a956d69df544f2bc787
wiiwiwi <wiiwiwi@naver.com>
2015-11-16 19:03:29 +0900

    Update intro-sunwall.lua
    1.2.3 패치 옮김

--- tome-krtr-v1.3.1.teaa/overload/data/texts/intro-sunwall.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/texts/intro-sunwall.lua
@@ -17,10 +17,10 @@
 -- Nicolas Casalini "DarkGod"
 -- darkgod@te4.org
 
-return [[Welcome #LIGHT_GREEN#@name@#WHITE#.
-You are a citizen of the Sunwall, and like many of your age you have been trained in combat and magic. It can be a harsh life, and though you dream at times of venturing to distant lands you know your duty is to help defend the outpost against the vicious onslaught from the Orc Prides. But now High Sun Paladin Aeryn has advised you of a new threat.
+return [[환영합니다, #LIGHT_GREEN#@name@#WHITE#.
+당신은 태양의 장벽 지역에 사는 시민이며, 당신 또래의 다른 사람들처럼 전투 기술과 마법 훈련을 받았습니다. 이 힘들고 혹독한 시간 동안 당신은 가끔씩 먼 곳을 여행하는 상상을 하기도 해봤지만, 저 사나운 오크 무리들의 맹공으로부터 전초기지를 지키는 것이 자신의 의무라는 것을 알고 있었기에 그 꿈을 포기하고는 했습니다. 그런데, 고위 태양의 기사 아에린이 당신에게 새로운 위협에 대해서 알려주었습니다.
 
-Nagas have been sighted in the Slazish Fens on the other side of the Sunwall Mountains. Their objective is unclear, but an assault from both sides would be truly devastating at such a pivotal time for your people. Though there are few forces to spare on the front line against the Prides, you have volunteered to investigate the naga threat and stop them if need be.
+그 위협은, 나가들이 태양의 장벽 산맥의 반대편에 위치한 슬라지쉬 늪지에서 목격되었다는 것입니다. 아직 그들의 목적은 불분명하지만, 양쪽에서 들어오는 공격은 이 중요한 순간에 치명적인 위협이 될 것이 분명합니다.비록 오크 무리와 대적할 병력도 부족하긴 하지만, 당신은 나가들의 위협을 조사하고 필요하다면 그 위협을 멈출 것을 자원했습니다.
 
-By the power of the Sun and the blessings of the Moons you step forth to find your fate.
+당신은 자신의 운명을 찾기 위해, 앞으로 걸음을 내딛었습니다. 태양의 힘과 달의 축복이 함께 하기를!
 ]]
