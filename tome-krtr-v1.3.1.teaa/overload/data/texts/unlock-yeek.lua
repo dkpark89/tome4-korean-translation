e3579f711c42661b7ad7360d74143f4c7bddd54f
blooo <cra7658@gmail.com>
2015-11-19 00:41:34 -0500

    Update unlock-yeek.lua

--- tome-krtr-v1.3.1.teaa/overload/data/texts/unlock-yeek.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/texts/unlock-yeek.lua
@@ -1,5 +1,5 @@
--- ToME - Tales of Maj'Eyal
--- Copyright (C) 2009 - 2015 Nicolas Casalini
+﻿-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009 - 2014 Nicolas Casalini
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -17,18 +17,18 @@
 -- Nicolas Casalini "DarkGod"
 -- darkgod@te4.org
 
-return "New Race: #LIGHT_GREEN#Yeek",
+return "새로운 종족 : #LIGHT_GREEN#이크",
 [[
-Yeeks are a mysterious race of small humanoids native to the tropical island of Rel.
-Their body is covered with white fur and their disproportionate heads give them a ridiculous look, yet they are a cunning and willful race.
-Although they are now nearly unheard of in Maj'Eyal, they spent many centuries as secret slaves to the Halfling nation of Nargol.
-They gained their freedom during the Age of Pyre and have since then followed 'The Way' - a unity of minds enforced by their powerful psionics.
+이크는 열대지역인 렐 출신의, 작고 신비한 소인 종족입니다.
+그들의 몸은 하얀 털로 덮혀있으며, 그들의 거대한 머리는 우스꽝스럽게 달려 있습니다. 그러나 그런 모습과는 달리, 그들은 교활하며 의지력이 있는 종족입니다.
+마즈'에이알 세계에서 그들에 대해 알려진 바는 거의 없지만, 그들은 수 세기 동안 하플링들의 나라인 나르골에서 비밀스럽게 노예로 살아왔습니다.
+그들은 장작더미의 시대에 자유를 얻었고, 그 때부터 ‘한길’ - 그들의 강력한 초능력으로 이루어진 정신의 단합체 - 를 따르기 시작했습니다.
 
-You have helped a Yeek Wayist and can now create a new character with the #LIGHT_GREEN#Yeek race#WHITE#.
+당신은 이크를 도와주었으며, 그 보답으로 이제 당신은 캐릭터를 만들 때 #LIGHT_GREEN#이크 종족#WHITE#을 선택할 수 있게 되었습니다.
 
-Race features:#YELLOW#
-- Mental domination racial power
-- Confusion resistance
-- Fast leveling
-- Frail body#WHITE#
+종족 특성:#YELLOW#
+- 종족의 힘을 이용한 정신 지배
+- 혼란 면역력
+- 빠른 성장
+- 허약한 몸#WHITE#
 ]]
