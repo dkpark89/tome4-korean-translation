01c42fd7ee6b103c170ac81e093909a089a2e7f7
Jolgame <jolgame@gmail.com>
2015-11-28 15:03:04 +0900

    Update ardhungol.lua
    1.2.3 작업

--- tome-krtr-v1.3.1.teaa/overload/data/lore/ardhungol.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/lore/ardhungol.lua
@@ -25,13 +25,14 @@ newLore{
 	id = "ardhungol-1",
 	category = "ardhungol",
 	name = "Rashim Journal (1)",
-	lore = [[#{bold}#Journal Entry I#{normal}#
+	kr_name = "라심의 여행 일지 (1)",
+	lore = [[#{bold}#여행 일지 I#{normal}#
 
-It's been some time since High Sun Paladin Aeryn sent me out on an assignment. According to recent intelligence, it would seem that a cavern has mysteriously emerged near our outpost, the Gates of Morning. They have bestowed the name "Ardhungol" on the newly discovered cavern. The elder warriors seem anxious in regards to the origin of this new subterranean area; they're likely concerned that the orcs are using Ardhungol as a staging point for further aggression against the Gates of Morning. I'll have to make sure I'm well equipped for this journey; the fate of us all could hang in the balance.
+고위 태양의 기사 아에린이 나를 밖으로 파견한지도 꽤 시간이 흘렀다. 최근 정보에 의하면, 그 동굴은 기이하게도 우리의 전초기지, 아침의 문 근처에서 솟아났다고 한다. 그들은 이 새롭게 발견된 동굴에 "알드훈골" 이라는 이름을 붙였다. 원로 전사들은 이 새로운 지하 지형의 기원에 관해서 염려하는 것으로 보인다. 아마 오크들이 아침의 문 공습의 발판으로 사용하려고 이 동굴을 만든건지에 대해 걱정하는 거겠지. 아무래도 내가 이 여행을 위해 철저히 준비했다는 것을 확신시켜줄 필요가 있을 것 같다. 우리의 운명은 앞날을 알 수 없는 상태이지만 말이다.
 
-#{bold}#Journal Entry II#{normal}#
+#{bold}#여행 일지 II#{normal}#
 
-I spoke with the border patrols tonight. There seem to have been several sightings of orcs and other wild beasts roaming throughout the wilderness, yet strangely enough, none seem to have been anywhere near Ardhungol. Perhaps the subterranean cavern was simply a natural phenomenon with unfortunate timing. It's either that, or an unknown force powerful enough to alter the very foundation of the land upon its arrival. I shudder at the latter thought, but for now this is all merely speculation. If all goes well, I'll be ready to depart by dawn.
+오늘 밤은 경계 순찰대와 이야기하였다. 그들은 몇몇 오크와 야생동물들을 목격했다고 한다. 기이하게도, 알드훈골 근처에서는 그 누구도 보이지 않았다고 한다. 아마 그 지하 동굴은 그저 운 없는 시기에 발생한 자연현상일 것이다. 아니면 지형조차 목적에 따라 바꿔버릴 수 있는, 어떤 강력한 힘이 작용했거나. 나는 후자에 대한 생각에 몸서리를 쳤으나, 일단 이 모든 것은 추측에 불과하다. 일이 잘 풀린다면, 새벽에는 도착할 수 있을 것 같다.
 ]]
 }
 
@@ -39,33 +40,34 @@ newLore{
 	id = "ardhungol-2",
 	category = "ardhungol",
 	name = "Rashim Journal (2)",
-	lore = [[#{bold}#Journal Entry III#{normal}#
+	kr_name = "라심의 여행 일지 (2)",
+	lore = [[#{bold}#여행 일지 III#{normal}#
 
-It was a half days journey from the Gates of Morning to Ardhungol, but I've finally arrived. I've decided to rest beneath the shade of a great oak tree and enjoy one of the sweet rolls my wife Melnela prepared for me. I'm unsure why, but the closer I've come to Ardhungol, the thicker the atmosphere has become, almost as if there is a foreign energy infecting the very air I breathe. I've been to many places in this vast world, but I've never felt an aura quite like this; I will have to uncover the source of this energy before I can bring a full report back to Aeryn.
+아침의 문에서 알드훈골까지 가기 위해 반나절 정도의 여행이 필요했지만, 마침내 도착하였다. 거대한 오크 나무의 그림자에 기대 휴식을 취하면서, 나의 아내 멜넬라가 준비해준 달콤한 롤빵을 즐기기로 하였다. 그 이유는 정확히 모르겠으나, 알드훈골에 가까워질수록 주변의 공기가 두꺼워지는 느낌이 든다. 거의 다른 곳의 힘이 영향을 미쳐, 숨쉬기조차 힘들정도로 말이다. 이 넓은 세계의 여러 장소에 있어봤지만, 이러한 기운은 처음 느껴본다. 아에린에게 결과 보고서를 제출하기 전에, 이 힘의 근원이 무엇인지를 반드시 밝혀내야만 할 것 같다.
 
-#{bold}#Journal Entry IV#{normal}#
+#{bold}#여행 일지 IV#{normal}#
 
-A thick green veil of mist permeates the air within this cavern; I'm uncertain if this is a byproduct of whatever creatures now inhabit this place, or if it's coming directly from another source. There also seem to be arcane rifts appearing in different locations throughout the cavern, though I dare not approach them; I will leave them for our anorithils to assess after I have secured the area. I must admit, I'm not looking forward to encountering the origin of a threat this grave, yet it is my sworn duty as a Sun Paladin to face what lies ahead; even should it be my own doom.
+두꺼운 초록색 먼지막이 동굴의 대기 중에 퍼져있다. 이것이 여기서 사는 생명체의 부산물인지, 아니면 다른 근원이 있는지 모르겠다. 또한 동굴 도처에 마법적 균열이 관찰되고 있다. 위험할지도 모르니 가까이 가지 않는 것이 좋을 것 같다. 저것들은 우선 주변 지역 안전을 확보한 뒤에, 우리의 아노리실들에게 조사를 맡겨야겠다. 인정하기는 싫지만, 역시 나는 이 무덤과도 같은 곳에 있는 위협의 근원을 만나고 싶지 않다. 이곳에 무엇이 있을지 밝혀내겠다고 태양의 기사로서 맹세를 했지만, 그 '무엇' 이 나 자신의 재앙을 불러올 수도 있으니까.
 
-#{bold}#Journal Entry V#{normal}#
+#{bold}#여행 일지 V#{normal}#
 
-Impossible; how could spiders have grown to such an immense size? Not only that, but the markings and physical traits are unlike any arachnid I have previously encountered. The first one I encountered nearly took my left arm during its initial assault, if my reflexes had been a split second slower, I fear the worst may have transpired. If the Gates of Morning are assaulted by not only the orcs, but also this new –found species of arachnids, I fear all hope will be lost. For the sake of all Eyal, I must venture further into this spider infested cavern and destroy the source of this incursion.
-]]
+불가능한 일이다. 어떻게 거미가 저렇게까지 거대해질 수가 있지? 그뿐만이 아니다. 무늬도, 육체적 능력도 내가 지금까지 봐온 어떤 거미와도 다르다. 내가 처음 만난 거미는 단 한번의 기습으로 내 왼쪽 팔을 가져갈 뻔 했다. 내 반응이 조금만 느렸더라면, 최악의 사태가 일어날 뻔 했다. 만약 아침의 문이 오크만이 아닌 이 새로운 거미들에게까지 공격을 받는다면, '마지막 희망' 은 산산조각나고 말 것이다. 에이알 세계를 위해서, 나는 이 거미놈들로 가득한 동굴을 더 탐사해서 습격의 근원을 찾아야만 한다.]] 
 }
 
 newLore{
 	id = "ardhungol-3",
 	category = "ardhungol",
 	name = "Rashim Journal (3)",
-	lore = [[#{bold}#Journal Entry VI#{normal}#
+	kr_name = "라심의 여행 일지 (3)",
+	lore = [[#{bold}#여행 일지 VI#{normal}#
 
-It took some time, but my muscles have started to adapt and react to the unique movements of these arachnid. The way they fight, it's almost as if they possess a higher level of thought, elevating them far beyond the base intelligence of lesser arachnid... is it possible that they could be sentient? Do they act individually, or are they perhaps manipulated by a greater arachnid? Let us pray that this species is not connected through a hive mind, for this threat could become even greater than I had imagined.
+시간이 조금 걸렸지만, 내 몸이 이 거미들의 독특한 움직임에 적응하기 시작했다. 놈들이 싸우는 방식은, 마치 고등 정신활동에 기반하고 있는 것처럼 보인다. 보통 거미들과는 차원이 다른... 정말로 거미가 지성을 가지는게 가능한 일인가? 그리고 놈들은 개별적으로 행동하는 것인가, 아니면 어떤 고등한 거미가 조종하는 것인가? 후자가 아니길 빌 수밖에 없다. 만약 후자라면 내가 상상할 수 있는 것보다도 더 큰 위협이 닥칠테니.
 
-#{bold}#Journal Entry VII#{normal}#
+#{bold}#여행 일지 VII#{normal}#
 
-I have slain dozens of these arachnids, each one seemingly more difficult than the last. I am unsure if this is a result of my weariness, or if they are also adapting to my movements and battle techniques. I don't see how that could be possible however, a mere spider couldn't possibly adapt to the battle techniques of a veteran Sun Paladin, it's simply not feasible. Furthermore, the origin of these spiders worries me. So far I have not found any other entrances to this cavern, meaning they were transported here via other means. Those arcane rifts I found earlier are starting to worry me, and I fear for us all when I think of what may be waiting for us beyond them. I must put these thoughts out of my mind; distractions will only serve to weaken my resolve.
+거의 10 마리에 가까운 거미들을 해치웠다. 마지막 놈은 다른 놈들보다 더 강력했다. 내 체력이 떨어져서 그렇게 느껴졌던 것인지, 아니면 놈들이 내 움직임과 전략에 적응하고 있어서인지, 확신하기 힘들다. 정말 놈들이 내 전략에 적응하고 있는 것일까? 당연하지만 평범한 거미는 숙련된 태양의 기사가 사용하는 전략에 적응할 수 없다. 불가능한 일이다. 게다가, 이 거미들이 대체 어디서 온 것인지에 대해 갈수록 불안감이 더해지고 있다. 이 동굴로 통하는 다른 입구는 전혀 발견하지 못했다. 그 뜻은, 놈들이 이곳으로 '전송' 됐거나 아니면 다른 방법을 사용했다는 것이다. 이전에 본 마법적 균열이 생각나는군. 그 균열 너머에 무엇이 우리를 기다리고 있을지 생각하기조차 두렵다. 일단은 빨리 이런 생각을 떨쳐내야겠다. 내 결의를 흔들리게 만들 뿐이니.
 
-#{bold}#Journal Entry VIII#{normal}#
+#{bold}#여행 일지 VIII#{normal}#
 
-Incredible, the size of that arachnid is unimaginable; there is no doubt about it, that spider is the matriarch here. I am heavily wounded, but there is no turning back now; I must finish what I started for the sake of all those awaiting my return. If I don't make it back, whoever is now reading this journal, please make High Sun Paladin Aeryn at the Gates of Morning aware of the grave situation... and tell Melnela that I'm sorry.]]
+세상에, 저 거미의 덩치는 정말 상상 이상이군. 의심할 여지 없이 저 거미가 놈들의 우두머리인게 확실하다. 하지만 나는 이미 심한 부상을 입었고, 이제 돌아갈 수도 없다. 나의 복귀를 기다리는 모든 자들을 위해서, 나는 이 일을 어떻게든 끝내야만 한다. 만약 내가 귀환하지 못하게 된다면, 이 일지를 읽는 누구라도 좋으니, 아침의 문에 있는 고위 태양의 기사 아에린에게 이 심각한 상황을 알려주기를... 그리고 멜넬라에게 미안하다고 전해주기를.]]
 }
