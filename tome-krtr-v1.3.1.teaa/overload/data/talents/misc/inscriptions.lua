c599d00a6567ff9e14a1ef8cc8e2c6681cb76c73
blooo <cra7658@gmail.com>
2015-12-04 11:29:48 -0500

    Update inscriptions.lua

--- tome-krtr-v1.3.1.teaa/overload/data/talents/misc/inscriptions.lua
+++ tome-krtr-v1.3.1.teaa/overload/data/talents/misc/inscriptions.lua
@@ -713,8 +713,9 @@ newInscription{
 		end },
 	requires_target = true,
 	range = 0,
+	radius = 6,
 	target = function(self, t)
-		return {type="cone", cone_angle=25, radius = 6, range=self:getTalentRange(t), talent=t, display={particle="bolt_ice", trail="icetrail"}}
+		return {type="cone", cone_angle=25, radius = self:getTalentRadius(t), range=self:getTalentRange(t), talent=t, display={particle="bolt_ice", trail="icetrail"}}
 	end,
 	action = function(self, t)
 		local data = self:getInscriptionData(t.short_name)
@@ -731,7 +732,7 @@ newInscription{
 			if not target or target == self then return end
 			
 			-- Minor damage, apply stun resist reduction, freeze
-			DamageType:get(DamageType.COLD).projector(target, tx, ty, DamageType.COLD, damage)
+			DamageType:get(DamageType.COLD).projector(self, tx, ty, DamageType.COLD, damage)
 			target:setEffect(target.EFF_WET, 5, {apply_power=data.inc_stat})
 			if target:canBe("stun") then
 				target:setEffect(target.EFF_FROZEN, 2, {hp=damage*1.5, apply_power=apply})
