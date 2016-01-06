4faa2014902fccf687ff09d6cee07d0b084430a1
wiiwiwi <wiiwiwi@naver.com>
2016-01-07 07:30:31 +0900

    Update summon-augmentation.lua

--- tome-krtr-v1.3.3.teaa/overload/data/talents/gifts/summon-augmentation.lua
+++ tome-krtr-v1.3.3.teaa/overload/data/talents/gifts/summon-augmentation.lua
@@ -19,6 +19,7 @@
 
 newTalent{
 	name = "Rage",
+	kr_name = "분노",
 	type = {"wild-gift/summon-augmentation", 1},
 	require = gifts_req1,
 	points = 5,
@@ -35,12 +36,13 @@ newTalent{
 		return true
 	end,
 	info = function(self, t)
-		return ([[Induces a killing rage in one of your summons, increasing all its stats by %d for 10 turns.]]):format(self:combatTalentMindDamage(t, 10, 100)/4)
+		return ([[하나의 소환수에게 살육의 본능을 심어, 10 턴 동안 모든 능력치가 %d 상승하게 됩니다.]]):format(self:combatTalentMindDamage(t, 10, 100)/4)
 	end,
 }
 
 newTalent{
 	name = "Detonate",
+	kr_name = "소환수 폭발",
 	type = {"wild-gift/summon-augmentation", 2},
 	require = gifts_req2,
 	points = 5,
@@ -58,7 +60,7 @@ newTalent{
 		local dt = self:getTalentFromId(target.wild_gift_detonate)
 
 		if not dt.on_detonate then
-			game.logPlayer("You may not detonate this summon.")
+			game.logPlayer("당신은 이 소환수를 폭발시킬 수 없습니다.")
 			return nil
 		end
 
@@ -79,37 +81,39 @@ newTalent{
 	end,
 	info = function(self, t)
 		local radius = self:getTalentRadius(t)
-		return ([[Destroys one of your summons, making it detonate in radius of %d.
-		- Ritch Flamespitter: Explodes into a fireball
-		- Hydra: Explodes into a ball of lightning, acid or poison
-		- Rimebark: Explodes into an iceball
-		- Fire Drake: Generates a cloud of fire
-		- War Hound: Explodes into a ball of physical damage
-		- Jelly: Explodes into a ball of slowing slime
-		- Minotaur: Explodes into a sharp ball, cutting all creatures
-		- Stone Golem: Knocks back all creatures
-		- Turtle: Grants a small shell shield to all friendly creatures
-		- Spider: Pins all foes around
-		In addition, a random summon will come off cooldown.
-		Hostile effects will not hit you or your other summons.
-		The effects improve with your Willpower.]]):format(radius)
+		return ([[소환수를 자폭시킵니다. 자폭은 주변 %d 칸 반경에 영향을 줍니다.
+		- 불꽃뿜는 릿치: 화염구 폭발
+		- 히드라: 번개나 산성 혹은 중독성 폭발
+		- 서리나무: 얼음 구체 폭발
+		- 화염 드레이크: 화염의 구름 생성
+		- 전투견: 물리 피해를 주는 폭발
+		- 젤리: 대상을 느리게 만드는 슬라임 덩어리 폭발
+		- 미노타우르스: 모든 생명체에게 출혈효과를 주는 날카로운 폭발
+		- 암석 골렘: 모든 생명체에게 밀어내기 효과
+		- 거북이: 우호적인 모든 생명체에게 작은 등껍질 방패 부여
+		- 거미: 주변의 모든 적들을 속박
+		추가적으로, 임의의 소환기술의 재사용 대기시간이 사라집니다.
+		적대적 효과는 시전자와 다른 소환수들에게는 영향을 주지 않습니다.
+		기술의 효과는 의지 능력치의 영향을 받아 증가합니다.]]):format(radius)
 	end,
 }
 
 newTalent{
 	name = "Resilience",
+	kr_name = "활력",
 	type = {"wild-gift/summon-augmentation", 3},
 	require = gifts_req3,
 	mode = "passive",
 	points = 5,
 	incCon = function(self, t) return math.floor(self:combatTalentScale(t, 2, 10, 0.75)) end,
 	info = function(self, t)
-		return ([[Improves all your summons' Constitution by %d, and adds %0.1f effective talent levels to your summon talents to determine your summons' lifetime.]]):format(t.incCon(self, t), self:getTalentLevel(t))
+		return ([[모든 소환수의 체격 능력치를 %d 상승시키고, 소환 기술들의 실질 기술 레벨을 %0.1f 만큼 더해 소환수의 지속 시간을 증가시킵니다. (증가된 소환 기술들의 실질 기술 레벨은 소환수의 지속 시간에만 영향을 주며, 소환수의 능력치는 증가되지 않습니다]]):format(t.incCon(self, t), self:getTalentLevel(t))
 	end,
 }
 
 newTalent{
 	name = "Phase Summon",
+	kr_name = "위치전환",
 	type = {"wild-gift/summon-augmentation", 4},
 	require = gifts_req4,
 	points = 5,
@@ -139,6 +143,6 @@ newTalent{
 		return true
 	end,
 	info = function(self, t)
-		return ([[Switches places with one of your summons. This disorients your foes, granting both you and your summon 50%% evasion for %d turns.]]):format(t.getDuration(self, t))
+		return ([[소환수 하나와 위치를 서로 바꿉니다. 이 기술을 사용하면 적이 당황하여, 시전자와 소환수 모두 %d 턴 동안 모든 근접 공격을 50%% 확률로 회피할 수 있게 됩니다.]]):format(t.getDuration(self, t))
 	end,
 }
